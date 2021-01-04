ff21d5489bc1b7e4468806f74d2bbbaf3b742a84
유정 <49063247+yujeongg@users.noreply.github.com>
2021-01-04 16:06:18 +0900

    axios 사용하여 비동기 데이터 가져오기

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
 
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    console.log("hello");
-  }
-  state = {
-    count : 0
-  }
-
-  add = () => {
-  this.setState(current => ({count:current.count + 1}));
-  } ;
-  minus =() => {
-    this.setState(current => ({count:this.state.count -1}));
+  state={
+    isLoading: true,
+    movies: [] // 미래에 있을 수도 있는 것을 적어둔 것, 필수 아님 (state에 default 값을 반드시 선언할 필요가 없다는 것.)
   };
 
+getMovies = async () => { // 오래 걸릴거야, 뭐가?
+  const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json"); // axios를 기다려
+}
+
   componentDidMount(){
-    console.log("component rendered");
-  }
-  
-  componentDidUpdate(){
-    console.log("i just updated");
-  }
+    this.getMovies();
+    }
 
   render () {
-    console.log("i'm rendering");
-    return(
-      <div>
-        <h1>hello : {this.state.count} </h1>
-        <button onClick = {this.add}>Add</button>
-        <button onClick = {this.minus}>Minus</button>
-      </div>
-    )
+    const {isLoading} = this.state;
+    return (
+    <div>
+      {isLoading ? "Loading..." : "We are ready"}
+    </div>
+    );
   }
 }
 export default App;
