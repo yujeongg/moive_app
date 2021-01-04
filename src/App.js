3c667482a700d226306fd6345e076229ec2fa9a1
유정 <49063247+yujeongg@users.noreply.github.com>
2021-01-04 15:02:23 +0900

    mounting,update,unmounting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("hello");
+  }
+  state = {
+    count : 0
+  }
+
+  add = () => {
+  this.setState(current => ({count:current.count + 1}));
+  } ;
+  minus =() => {
+    this.setState(current => ({count:this.state.count -1}));
+  };
+
+  componentDidMount(){
+    console.log("component rendered");
+  }
+  
+  componentDidUpdate(){
+    console.log("i just updated");
+  }
+
+  render () {
+    console.log("i'm rendering");
+    return(
+      <div>
+        <h1>hello : {this.state.count} </h1>
+        <button onClick = {this.add}>Add</button>
+        <button onClick = {this.minus}>Minus</button>
+      </div>
+    )
+  }
 }
-
 export default App;
